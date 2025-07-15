6cd3c5e076c245f09a02a8b752cbe91a5e938b75
mathewpeedikayil <mathewphilip.peedikayil@gmail.com>
2025-07-15 22:05:23 +0530

    created predict button

--- app.py
+++ app.py
@@ -1,16 +1,15 @@
 import streamlit as st
 import pandas as pd
+import joblib
 
 df = pd.read_csv("data.csv")
-
 # ['area', 'bedrooms', 'bathrooms', 'stories', 'parking']
-area_min = df["area"].min()
+
 bedrooms_min = df["bedrooms"].min()
 bathrooms_min = df["bathrooms"].min()
 stories_min = df["stories"].min()
 parking_min = df["parking"].min()
 
-area_max = df["area"].max()
 bedrooms_max = df["bedrooms"].max()
 bathrooms_max = df["bathrooms"].max()
 stories_max = df["stories"].max()
@@ -18,12 +17,26 @@ parking_max = df["parking"].max()
 
 st.title("House Price Prediction 🏠")
 
-area_sldr = st.slider("Area (sq ft)", area_min, area_max, 0)
+area_num_inpt = st.number_input("Area (sq ft)")
 bedrooms_sldr = st.slider("Number of bedrooms", bedrooms_min, bedrooms_max, 0)
 bathrooms_sldr = st.slider("Number of bathrooms", bathrooms_min, bathrooms_max, 0)
 stories_sldr = st.slider("Number of stories", stories_min, stories_max, 0)
 parking_sldr = st.slider("Number of parking spaces", parking_min, parking_max, 0)
 
+# Sample Input
+# 7420, 4, 2, 3, 2
+input_data = pd.DataFrame([{
+    'area': area_num_inpt,
+    'bedrooms': bedrooms_sldr,
+    'bathrooms': bathrooms_sldr,
+    'stories': stories_sldr,
+    'parking': parking_sldr
+}])
+
+lm = joblib.load('model.pkl')
+if st.button("Predict House Price"):
+    predicted_price = lm.predict(input_data)
+    st.success(f"💰 Predicted House Price: $ {predicted_price[0]:,.0f}")
 
 # streamlit run app.py
 
